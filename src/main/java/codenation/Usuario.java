da4d1ab4af2c760496ca4d10747677f17ae31baf
Rubens Lima <rubens_plima@hotmail.com>
2019-02-02 11:50:44 -0300

    Classe usuario - rubens.lima

---
+++ src/main/java/codenation/Usuario.java
@@ -0,0 +1,34 @@
+package codenation;
+
+public class Usuario {
+	
+	private Integer id;
+	private String nome;
+	private String cpf;
+	
+	public Usuario(Integer id, String nome, String cpf) {
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+}
